947c49e57a68caabb1cfd9341c29705543c55966
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-09-14 11:20:19 -0300

    changing audio links address

--- js/sounds.js
+++ js/sounds.js
@@ -1,10 +1,10 @@
 export default function() {
     const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
     const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
-    const bgAudioForest = new Audio("../audio/forest.wav")
-    const bgAudioRain = new Audio("../audio/rain.wav")
-    const bgAudioCoffeeShop = new Audio("../audio/coffee-shop.wav")
-    const bgAudioFireplace = new Audio("../audio/fireplace.wav")
+    const bgAudioForest = new Audio("https://github.com/madalena-rocha/focus-timer-2.0/blob/main/audio/forest.wav")
+    const bgAudioRain = new Audio("https://github.com/madalena-rocha/focus-timer-2.0/blob/main/audio/rain.wav")
+    const bgAudioCoffeeShop = new Audio("https://github.com/madalena-rocha/focus-timer-2.0/blob/main/audio/coffee-shop.wav")
+    const bgAudioFireplace = new Audio("https://github.com/madalena-rocha/focus-timer-2.0/blob/main/audio/fireplace.wav")
 
     bgAudioForest.loop = true
     bgAudioRain.loop = true
