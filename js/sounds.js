1eb9ac764c9c8cb6705264b0b0be4d696914dc41
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-09-17 15:41:27 -0300

    Update sounds.js

--- js/sounds.js
+++ js/sounds.js
@@ -9,7 +9,7 @@ export default function() {
     const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
     const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
     const bgAudioForest = new Audio("https://github.com/madalena-rocha/focus-timer-2.0/blob/main/audio/forest.wav?raw=true")
-    const bgAudioRain = new Audio("../audio/rain.wav")
+    const bgAudioRain = new Audio("https://github.com/madalena-rocha/focus-timer-2.0/blob/main/audio/rain.wav?raw=true")
     const bgAudioCoffeeShop = new Audio("https://github.com/madalena-rocha/focus-timer-2.0/blob/main/audio/coffee-shop.wav?raw=true")
     const bgAudioFireplace = new Audio("https://github.com/madalena-rocha/focus-timer-2.0/blob/main/audio/fireplace.wav?raw=true")
 
